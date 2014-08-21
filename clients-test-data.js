24a79219e1f0fed387b2ba0ec9164d3d1f17802f
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2014-08-20 21:50:56 -0400

    PT-1 list clients

---
+++ clients-test-data.js
@@ -0,0 +1,118 @@
+var clients = {
+	"data": [{
+		"Client Name": "Joanne cleins",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client1"
+	}, {
+		"Client Name": "Brenda Jones",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client2"
+	}, {
+		"Client Name": "Susan Elms",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client3"
+	}, {
+		"Client Name": "Roger Bills",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client4"
+	}, {
+		"Client Name": "Cindy Cole",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client5"
+	}, {
+		"Client Name": "Susan Jones",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client6"
+	}, {
+		"Client Name": "Abby Stanciki",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client7"
+	}, {
+		"Client Name": "Nicholas Roberts",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client8"
+	}, {
+		"Client Name": "Trent Telman",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client9"
+	}, {
+		"Client Name": "Stephanie Rowe",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client11"
+	}, {
+		"Client Name": "Bill McGillis",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client12"
+	}, {
+		"Client Name": "Clive Custer",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client13"
+	}, {
+		"Client Name": "Roy Hibbs",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client15"
+	}, {
+		"Client Name": "Gene Simons",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client18"
+	}, {
+		"Client Name": "Geoff Slater",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client19"
+	}, {
+		"Client Name": "Brenda Jones",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client21"
+	}, {
+		"Client Name": "Jeff Smith",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client32"
+	}, {
+		"Client Name": "Jill Reys",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client38"
+	}, {
+		"Client Name": "Jill Malloy",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client39"
+	}, {
+		"Client Name": "Stu Shafeer",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client40"
+	}, {
+		"Client Name": "Carol Mcguffy",
+		"Email": "",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client41"
+	}, {
+		"Client Name": "Mickey Coke",
+		"Email": "mickey@thecokes.net",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client42"
+	}, {
+		"Client Name": "Bradley Trager",
+		"Email": "bradleytrager@gmail.com",
+		"fk Studio ID": "studio4",
+		"pkClientID": "client1195"
+	}]
+};

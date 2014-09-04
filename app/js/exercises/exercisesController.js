04101a0e607526cbc6e5e4b235a03c57325b3afd
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2014-09-03 22:27:38 -0400

    added client endpoint

--- app/js/exercises/exercisesController.js
+++ app/js/exercises/exercisesController.js
@@ -7,14 +7,20 @@ define([
 	// Declare app level module which depends on filters, and services
 	var exercisesController = function($scope, $http, $routeParams) {
 		var BASE_URL = 'http://pt.trackformance.com/RESTfm/PT_Demo/script/';
-		$http.get(BASE_URL + 'listExercisesForClient/json-exerciseList/.json?RFMscriptParam=' + $routeParams.clientId).then(function(exercises) {
-			$scope.exercises = exercises.data.data;
-			console.log(exercises);
-			$scope.exercises.forEach(function(exercise) {
-				exercise.id = exercise['pk Exercise ID'];
-				exercise.name = exercise['Exercise Name'];
-			})
-		});
+		$http.get(BASE_URL + 'listUser/json-user/.json?RFMscriptParam=' + $routeParams.clientId)
+			.then(function() {
+				$http.get(BASE_URL + 'listExercisesForClient/json-exerciseList/.json?RFMscriptParam=' + $routeParams.clientId)
+					.then(function(exercises) {
+						$scope.exercises = exercises.data.data;
+						console.log(exercises);
+						$scope.exercises.forEach(function(exercise) {
+							exercise.id = exercise['pk Exercise ID'];
+							exercise.name = exercise['Exercise Name'];
+						});
+					});
+			});
+
+
 	};
 
 	return ['$scope', '$http', '$routeParams', exercisesController];
--- app/js/mockBackend/mockBackendRunner.js
+++ app/js/mockBackend/mockBackendRunner.js
@@ -9,9 +9,12 @@ define([
 	var mockBackendRunner = function($httpBackend) {
 		var BASE_URL = 'http://pt.trackformance.com/RESTfm/PT_Demo/script/';
 		var clientListURL = BASE_URL + 'listClientsForLoggedInUser/json-user/.json';
+		var clientURL = BASE_URL + 'listUser/json-user/.json?'
+									+'RFMscriptParam=client39';
 		var clientExercisesURL = BASE_URL + 'listExercisesForClient/json-exerciseList/.json?' 
-											+ 'RFMscriptParam=client39';
+									+ 'RFMscriptParam=client39';
 		$httpBackend.whenGET(clientExercisesURL).respond(clientExercises);
+		$httpBackend.whenGET(clientURL).respond(clients);
 		$httpBackend.whenGET(clientListURL).respond(clients);
 		$httpBackend.whenGET(/partials\//).passThrough();
 	

4f4b057aa954871b099469edde7024eb81f25418
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2014-09-22 22:18:57 -0400

    add tables

--- app/js/clients/exercisesController.js
+++ app/js/clients/exercisesController.js
@@ -42,10 +42,21 @@ define([], function() {
 		};
 
 		$scope.getReport = function(exerciseId) {
-			clientsService.getReport(exerciseId).then(function(report) {
-				var _report = report.data.data;
+			clientsService.getReport(exerciseId).then(function(response) {
+				var report = response.data.data;
+				console.log(report);
 				var dates = [];
-				_report.forEach(function(data, index) {
+
+				var timestamps = [];
+				var goToFailures = [];
+				var performances = [];
+				var currentWeights = [];
+				var netChangeWeights = [];
+				var timeUnderLoads = [];
+				var rangeOfMotions = [];
+				var outOfSequences = [];
+				
+				report.forEach(function(data, index) {
 					data["Date"] = new Date(data["Date"]).getTime();
 					data["dateIndex"] = index;
 					dates.push(data["dateIndex"]);
@@ -53,75 +64,86 @@ define([], function() {
 					var time = 60 * parseInt(timeParts[1]) + parseInt(timeParts[2]);
 					data["Total Time"] = time;
 					data["Current Weight"] = parseInt(data["Current Weight"]);
+
+					timestamps.push(data["Timestamp"]);
+					goToFailures.push("N/A");
+					outOfSequences.push("N/A");
+					performances.push(data["performanceView"]);
+					currentWeights.push(data["Current Weight"]);
+					netChangeWeights.push(data["netChangeWeight"]);
+					timeUnderLoads.push(data["Total Time"]);
+					rangeOfMotions.push(data["Current Range"]);
+
 				});
 
+				$scope.data = report;
+				$scope.timestamps = timestamps;
+				$scope.goToFailures = goToFailures;
+				$scope.performances = performances;
+				$scope.outOfSequences = outOfSequences;
+				$scope.currentWeights = currentWeights;
+				$scope.netChangeWeights = netChangeWeights;
+				$scope.timeUnderLoads = timeUnderLoads;
+				$scope.rangeOfMotions = rangeOfMotions;
 
-
-				$scope.data = _report;
-				console.log($scope.data);
 				$scope.options = {
-			series: [{
-				y: "Current Weight",
-				label: "Current Weight",
-				color: "#1871A4",
-				axis: "y2",
-				type: "line",
-				thickness: "3px",
-				id: "series_0"
-			}, {
-				y: "Total Time",
-				label: "Time Under Load",
-				color: "#BFBFBF",
-				type: "column",
-				axis: "y",
-				id: "series_1"
-			}],
-			stacks: [],
-			axes: {
-				x: {
-					type: "linear",
-					key: "dateIndex",
-					labelFunction: function(index) {
-						if (dates.indexOf(index) == -1) {
-							return "";
+					series: [{
+						y: "Current Weight",
+						label: "Current Weight",
+						color: "#1871A4",
+						axis: "y2",
+						type: "line",
+						thickness: "3px",
+						dotSize: 10,
+						id: "series_0"
+					}, {
+						y: "Total Time",
+						label: "Time Under Load",
+						color: "#BFBFBF",
+						type: "column",
+						axis: "y",
+						id: "series_1"
+					}],
+					axes: {
+						x: {
+							type: "linear",
+							key: "dateIndex",
+							labelFunction: function(index) {
+								if (dates.indexOf(index) == -1) {
+									return "";
+								}
+								var msTime = $scope.data[index].Date
+								var date = new Date(msTime);
+
+								var dateParts = date.toString().split(" ");
+								var formattedDate = dateParts[1] + dateParts[2];
+								return formattedDate;
+							}
+						},
+						y: {
+							type: "linear",
+							labelFunction: function(seconds) {
+								var seconds = parseInt(seconds);
+								var minutes = Math.floor(seconds / 60);
+								var remainingSeconds = seconds - 60 * minutes;
+								return minutes.toString() + ":" + remainingSeconds.toString();
+							}
+						},
+						y2: {
+							type: "linear",
+							labelFunction: function(weight) {
+								return weight + " lbs"
+							}
 						}
-						var msTime = $scope.data[index].Date
-						var date = new Date(msTime);
-
-						var dateParts = date.toString().split(" ");
-						var formattedDate = dateParts[1] + dateParts[2];
-						return formattedDate;
-					}
-				},
-				y: {
-					type: "linear",
-					labelFunction: function(seconds) {
-						var seconds = parseInt(seconds);
-						var minutes = Math.floor(seconds / 60);
-						var remainingSeconds = seconds - 60 * minutes;
-						return minutes.toString() + ":" + remainingSeconds.toString();
-					}
-				},
-				y2: {
-					type: "linear",
-					labelFunction: function(weight) {
-						return weight + " lbs"
-					}
-				}
-			},
-			lineMode: "linear",
-			tension: 0.7,
-			tooltip: {
-				mode: "scrubber"
-			},
-			drawLegend: true,
-			drawDots: true,
-			columnsHGap: 5
-		};
-
+					},
+					lineMode: "linear",
+					tension: 0.7,
+					drawLegend: true,
+					drawDots: true,
+					columnsHGap: 5
+				};
 			});
 		};
-
 
 	};
 

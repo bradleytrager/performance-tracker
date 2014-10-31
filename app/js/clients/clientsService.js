239dfc4a94997215a47e9006fe39e9cd2022e72b
Bradley Trager <bradleytrager@Bradleys-MacBook-Pro.local>
2014-10-30 22:32:52 -0400

    PT-10 adjusted axes to +-10% min and max

--- app/js/clients/clientsService.js
+++ app/js/clients/clientsService.js
@@ -6,7 +6,7 @@ define([], function() {
 		var service = this;
 		var STAGE_URL = 'PT_Demo';
 		var PROD_URL = 'PerfTracker17austin';
-		var BASE_URL = 'http://pt.trackformance.com/RESTfm/' + PROD_URL + '/script/';
+		var BASE_URL = 'http://pt.trackformance.com/RESTfm/' + STAGE_URL + '/script/';
 
 		this.get = function() {
 			return $http.get(BASE_URL + 'listClientsForLoggedInUser/json-user/.json');
